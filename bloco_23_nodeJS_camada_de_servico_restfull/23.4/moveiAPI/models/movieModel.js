441566a56a1ed0f54d3bdb8ec8c7f4ef66a69c99
Gustavo Silva <gusttavo.x.santos@gmail.com>
2022-08-15 19:46:25 -0300

    feat: Exercicios de fixação incompletos 23.4

--- bloco_23_nodeJS_camada_de_servico_restfull/23.4/moveiAPI/models/movieModel.js
+++ bloco_23_nodeJS_camada_de_servico_restfull/23.4/moveiAPI/models/movieModel.js
@@ -9,6 +9,16 @@ const create = async ({ title, directedBy, releaseYear }) => {
   return { id: result.insertId };
 };
 
+const getById = async (id) => {
+  const [result] = await db.execute('SELECT * FROM model_example.movies WHERE id = ?', [
+    id,
+  ]);
+
+  if (result.length === 0) return null;
+  return result[0];
+};
+
 module.exports = {
   create,
+  getById,
 };
--- bloco_23_nodeJS_camada_de_servico_restfull/23.4/moveiAPI/tests/models/movieModel.test.js
+++ bloco_23_nodeJS_camada_de_servico_restfull/23.4/moveiAPI/tests/models/movieModel.test.js
@@ -16,10 +16,10 @@ describe('Insere um novo filme no BD', () => {
     sinon.stub(db, 'execute').resolves(execute);
   });
 
-    // Restauraremos a função `execute` original após os testes.
-    after(async () => {
-      db.execute.restore();
-    });
+  // Restauraremos a função `execute` original após os testes.
+  after(async () => {
+    db.execute.restore();
+  });
 
   describe('Quando é inserido com sucesso', () => {
     it('retorna um objeto', async () => {
@@ -30,6 +30,54 @@ describe('Insere um novo filme no BD', () => {
     it('tal objeto possui um "id" do novo filme inserido', async () => {
       const response = await moviesModel.create(payloadMovie);
       expect(response).to.have.property('id');
+    });
+  });
+});
+
+describe.only('Pega um filme pelo ID no DB', () => {
+  const id = 1;
+
+  const expectedResult = {
+    id: 1,
+    nome: 'A volta dos que não foram',
+    direcao: 'Gustavo da SIlva',
+    data_de_lancamento: '1999',
+  };
+
+  describe('Quando é passado um ID existente', async () => {
+    before(async () => {
+      const execute = expectedResult; // retorno esperado nesse teste
+      sinon.stub(db, 'execute').resolves(execute);
+    });
+
+    after(async () => {
+      db.execute.restore();
+    });
+
+    it('O resultado deve ser um objeto', async () => {
+      const result = await moviesModel.getById(id);
+      expect(result).to.be.an('object');
+    });
+
+    it('O resultado deve ser o objeto esperado', async () => {
+      const result = await moviesModel.getById(id);
+      expect(result).to.be.deep.equal(expectedResult);
+    });
+
+    describe('Quando é passado um ID inexistente', async () => {
+      before(async () => {
+        const execute = [[]]; // retorno esperado nesse teste
+        sinon.stub(db, 'execute').resolves(execute);
+      });
+
+      after(async () => {
+        db.execute.restore();
+      });
+
+      it('retorna null', async () => {
+        const result = await moviesModel.getById(100);
+        expect(result).to.be.equal(null);
+      });
     });
   });
 });
