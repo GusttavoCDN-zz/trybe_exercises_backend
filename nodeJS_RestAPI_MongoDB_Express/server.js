e84c61c637fd2a9a680def409fc897e5e8d85bba
Gustavo Santos <gusttavo.x.santos@gmail.com>
2022-08-23 18:11:57 +0000

    feat: Criado primeiras rotas de teste

--- nodeJS_RestAPI_MongoDB_Express/server.js
+++ nodeJS_RestAPI_MongoDB_Express/server.js
@@ -1,11 +1,4 @@
-const http = require('http');
-const port = 3000;
+import app from './src/app.js';
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
-  res.end('Teste de Servidor com http');
-});
-
-server.listen(port, () => {
-  console.log(`Server is running on the port ${port}`);
-});
+app.listen(port, () => console.log('Server is listening on port ' + port));
