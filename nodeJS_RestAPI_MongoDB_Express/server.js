bf9f4aa2a2203e6a618ec92159b02ee3f6186b9e
Gustavo Santos <gusttavo.x.santos@gmail.com>
2022-08-23 18:37:36 +0000

    feat: Feito crud basico de testes para a API

--- nodeJS_RestAPI_MongoDB_Express/server.js
+++ nodeJS_RestAPI_MongoDB_Express/server.js
@@ -1,3 +1,4 @@
+import express from 'express';
 import app from './src/app.js';
 const port = process.env.PORT || 3000;
 
--- nodeJS_RestAPI_MongoDB_Express/src/app.js
+++ nodeJS_RestAPI_MongoDB_Express/src/app.js
@@ -1,17 +1,40 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 const books = [
   { id: 1, title: 'The Lord of the Rings' },
   { id: 2, title: 'The Hobbit' },
 ];
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome');
+app.get('/books', (req, res) => {
+  res.status(200).json(books);
 });
 
-app.get('/books', (req, res) => {
+app.get('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const index = books.findIndex(({ id: bookId }) => bookId === Number(id));
+  res.status(200).json(books[index]);
+});
+
+app.post('/books', (req, res) => {
+  const book = req.body;
+  books.push(book);
+  res.status(200).json({ message: 'Livro cadastrado com sucesso' });
+});
+
+app.put('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const index = books.findIndex(({ id: bookId }) => bookId === Number(id));
+  books[index].title = req.body.title;
+  res.status(200).json(books);
+});
+
+app.delete('/books/:id', (req, res) => {
+  const { id } = req.params;
+  const index = books.findIndex(({ id: bookId }) => bookId === Number(id));
+  books.splice(index, 1);
   res.status(200).json(books);
 });
 
