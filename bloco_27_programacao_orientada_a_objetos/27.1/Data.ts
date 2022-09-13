7562b5276c98b732a7a6caa2dcf30e3aa31933dc
Gustavo Santos <gusttavo.x.santos@gmail.com>
2022-09-13 17:13:21 +0000

    feat: Realizado todos os exercicios do dia 27.1

--- bloco_27_programacao_orientada_a_objetos/27.1/Data.ts
+++ bloco_27_programacao_orientada_a_objetos/27.1/Data.ts
@@ -4,12 +4,56 @@ export interface IData {
   year: number;
 }
 
+enum Months {
+  January = 1,
+  February = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12,
+}
+
 export class Data implements IData {
   private _day: number;
   private _month: number;
   private _year: number;
+  private monthNames = Months;
 
-  constructor(day: number, month: number, year: number) {
+  private static privateIsLeapYear = (year: number): boolean => {
+    if (year % 4 === 0 && year % 100 !== 0) return true;
+    if (year % 400 === 0) return true;
+    return false;
+  };
+
+  private static validateDay = (day: number, month: number, year: number): boolean => {
+    if (month === 2) {
+      if (Data.privateIsLeapYear(year)) return day <= 29;
+      return day <= 28;
+    }
+    if (month === 4 || month === 6 || month === 9 || month === 11) return day <= 30;
+    return day <= 31;
+  };
+
+  private static validateRawData(rawData: string): boolean {
+    const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    return regex.test(rawData);
+  }
+
+  constructor(rawData: string) {
+    const isDataFormatValid = Data.validateRawData(rawData);
+    if (!isDataFormatValid) throw new Error('The data must have the format dd/mm/yyyy');
+
+    const [day, month, year] = rawData.split('/').map(Number);
+    const isDayValid = Data.validateDay(day, month, year);
+
+    if (!isDayValid) throw new Error('Invalid day');
+
     this.day = day;
     this.month = month;
     this.year = year;
@@ -20,8 +64,9 @@ export class Data implements IData {
   }
 
   set day(value: number) {
-    if (value >= 1 && value <= 31) this._day = value;
-    else throw new Error('Invalid day');
+    const isDayValid = Data.validateDay(value, this.month, this.year);
+    if (!isDayValid) throw new Error('Invalid day');
+    else this._day = value;
   }
 
   get month(): number {
@@ -40,4 +85,18 @@ export class Data implements IData {
   set year(value: number) {
     this._year = value;
   }
+
+  public getMonthName = (): string => this.monthNames[this.month];
+
+  public isLeapYear = (): boolean => Data.privateIsLeapYear(this.year);
+
+  public compare = ({ day, month, year }: Data) => {
+    if (this.year === year && this.month === month && this.day === day) return 0;
+    if (this.year > year) return 1;
+    if (this.year < year) return -1;
+    if (this.month > month) return 1;
+    if (this.month < month) return -1;
+    if (this.day > day) return 1;
+    if (this.day < day) return -1;
+  };
 }
